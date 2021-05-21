aa9645ae9252ec8813163384c0b42e1bb77dda63
cakinalperen <36760046+cakinalperen@users.noreply.github.com>
2021-05-21 18:26:37 +0300

    update

--- src/components/Main.js
+++ src/components/Main.js
@@ -57,6 +57,45 @@ class Main extends Component {
                       <ul id="postList" className="list-group list-group-flush">
                         <li className="list-group-item">
                           <p>Q: {post.content}</p>
+                         
+                          {post.replies.map((p,k)=>{ return(<div className="card mb-4" key={k}>
+                          
+                          <div className="card-header">
+                            <img
+                              className='mr-2'
+                              width='30'
+                              height='30'
+                              src={`data:image/png;base64,${new Identicon(post.author, 30).toString()}`}
+                            />
+                            <small className="text-muted">{p.author}</small>
+                          </div>
+                          <ul id="postList" className="list-group list-group-flush">
+                            <li className="list-group-item">
+                          {p.content}
+                          </li>
+                          
+                           <li key={key} className="list-group-item py-2">
+                          <small className="float mt-1 text-muted">
+                            Votes: {window.web3.utils.fromWei(p.tipAmount.toString(), 'Ether')} ETH
+                          </small>
+                  
+                          <button
+                            className="btn btn-link btn-sm float-right pt-0"
+                            name={p.id}
+                            onClick={(event) => {
+                              let tipAmount = window.web3.utils.toWei('0.1', 'Ether')
+                              console.log(event.target.name, tipAmount)
+                              this.props.tipReply(event.target.name, tipAmount)
+                            }}
+                          >
+                            UPVOTE
+                          </button>
+              
+                        </li>
+                          </ul>
+                          
+                          </div>) })}
+                          
                         </li>
                         <li key={key} className="list-group-item py-2">
                           <small className="float mt-1 text-muted">
